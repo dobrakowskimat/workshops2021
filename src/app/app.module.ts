8fa01f6f596c12eaf7103ffbeb04e07746155c5f
M. Dobrakowski (c) <m.dobrakowski@avanade.com>
2021-11-18 11:33:33 +0100

    after 3rd workshops

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { UserListComponent } from './components/user-list/user-list.component';
 import { FormsModule } from '@angular/forms';
+import { UserComponent } from './components/user/user.component';
 
 @NgModule({
-  declarations: [AppComponent, UserListComponent],
+  declarations: [AppComponent, UserListComponent, UserComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
