420a187a58c3a700d3b538fb6e12c01f648eb3ab
szymeq17 <rysz.szymon@gmail.com>
2021-12-07 17:00:27 +0100

    Add book form

--- src/app/components/edit-book-form/edit-book-form.component.ts
+++ src/app/components/edit-book-form/edit-book-form.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import {BookService} from "../../services/book.service";
+import {MatDialogRef} from "@angular/material/dialog";
 
 @Component({
   selector: 'app-edit-book-form',
@@ -9,11 +11,21 @@ import { FormControl, FormGroup } from '@angular/forms';
 export class EditBookFormComponent implements OnInit {
   bookForm: FormGroup;
 
-  constructor() {
+  constructor(private readonly bookService: BookService,
+              private dialogRef: MatDialogRef<EditBookFormComponent>) {
+    this.dialogRef = dialogRef
     this.bookForm = new FormGroup({
-      title: new FormControl('supre title'),
+      title: new FormControl('Book title'),
+      authorFirstName: new FormControl("First name"),
+      authorLastName: new FormControl("Last name"),
+      publicationDateUtc: new FormControl("1900-01-31T00:00:00"),
+      isbn: new FormControl("12345678")
     });
   }
 
+  addBook(): void {
+    this.bookService.addBook(this.bookForm.value).subscribe()
+    this.dialogRef.close()
+  }
   ngOnInit(): void {}
 }
