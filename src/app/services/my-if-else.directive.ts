fc7f3d23ef1aa1300511361f55c8527585bbcf5b
Dawid Dudek <dawid.dudek@fingo.pl>
2022-01-25 17:06:30 +0100

    fix if else

--- src/app/services/my-if-else.directive.ts
+++ src/app/services/my-if-else.directive.ts
@@ -10,12 +10,16 @@ export class MyIfElseDirective {
     private viewContainer: ViewContainerRef
   ) { }
 
+  @Input('appMyIfElseThen')
+  elseTemplate: TemplateRef<any> | null = null;
+
   @Input() set appMyIfElse(condition: boolean){
     if (condition) {
       this.viewContainer.clear();
       this.viewContainer.createEmbeddedView(this.templateRef);
-    } else if (!condition) {
+    } else if (!condition && this.elseTemplate !== null) {
       this.viewContainer.clear();
+      this.viewContainer.createEmbeddedView(this.elseTemplate)
     }
   }
 
